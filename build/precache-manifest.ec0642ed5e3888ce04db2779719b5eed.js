14937f77c8edf621d7197722f48acdf9fcd562c6
Amanda Yonce <amanda.stork@gmail.com>
2020-01-31 12:34:26 -0500

    changed ignore file

---
+++ build/precache-manifest.ec0642ed5e3888ce04db2779719b5eed.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a352c53aea7ed90642daf9a56833236a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9446086681215edaadbb",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "df58d8f36997d1e8090a",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9446086681215edaadbb",
+    "url": "/portfolio/static/js/2.46790181.chunk.js"
+  },
+  {
+    "revision": "339a5e5189e6b5cea126914451abf1a1",
+    "url": "/portfolio/static/js/2.46790181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df58d8f36997d1e8090a",
+    "url": "/portfolio/static/js/main.5b6be3b7.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "c9cfe07b4e4e65b1f846f0322c5c22bb",
+    "url": "/portfolio/static/media/bootstrapicon.c9cfe07b.png"
+  },
+  {
+    "revision": "9fe7584b6d29bec74a66024829929697",
+    "url": "/portfolio/static/media/cssicon.9fe7584b.png"
+  },
+  {
+    "revision": "b15eedbdafbbdbca3249e3942f4faf3b",
+    "url": "/portfolio/static/media/githubicon.b15eedbd.png"
+  },
+  {
+    "revision": "16e95eacf49fe829ce41c43f568e0a71",
+    "url": "/portfolio/static/media/gitlabicon.16e95eac.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/portfolio/static/media/htmlicon.2ea01c0e.png"
+  },
+  {
+    "revision": "ef042da13ae2ca65067ab2ffc6fd537f",
+    "url": "/portfolio/static/media/jsicon.ef042da1.jpeg"
+  },
+  {
+    "revision": "056b2967ab1371bed46395f89a81185d",
+    "url": "/portfolio/static/media/mainIMG.056b2967.jpg"
+  },
+  {
+    "revision": "c3eb07e7eca575c8b9d69dfe1f6f38dd",
+    "url": "/portfolio/static/media/myAvatar.c3eb07e7.png"
+  },
+  {
+    "revision": "3346eb0f317b315800c66d188173837f",
+    "url": "/portfolio/static/media/nodejsicon.3346eb0f.png"
+  },
+  {
+    "revision": "cbb99608ddbfaa82fc882fde4e51273e",
+    "url": "/portfolio/static/media/reacticon.cbb99608.png"
+  }
+]);
