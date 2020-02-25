98c81e502d2ed5d641af37295575ae2689d6d9f0
Amanda Yonce <amanda.stork@gmail.com>
2020-02-25 15:17:56 -0500

    Updates

--- precache-manifest.0a95012ac5932814133c03be14bc630f.js
+++ precache-manifest.0a95012ac5932814133c03be14bc630f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ececa14fc93db475bc79e11635980d8",
+    "revision": "5736ee4f1a43448b1b0a94a3ae88472c",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "c32f41cec8d3c921d34f",
+    "revision": "c5d7d6d8affde75d3bab",
     "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.bb0e30fe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c32f41cec8d3c921d34f",
-    "url": "/portfolio/static/js/main.fe9dc996.chunk.js"
+    "revision": "c5d7d6d8affde75d3bab",
+    "url": "/portfolio/static/js/main.fa2a31a0.chunk.js"
   },
   {
     "revision": "0d98471f6c3b69ca4251",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0e33a6af52167077106cfe575f2e8260.js"
+  "/portfolio/precache-manifest.0a95012ac5932814133c03be14bc630f.js"
 );
 
 self.addEventListener('message', (event) => {
