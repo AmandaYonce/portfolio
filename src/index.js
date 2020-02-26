95a58636a9b95e22c8e1dacd1c17b9a7818a83bd
Amanda Yonce <amanda.stork@gmail.com>
2020-02-25 20:01:23 -0500

    index

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL+ "/"}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
          <App/> 
     </BrowserRouter>,
  document.getElementById("root"));
